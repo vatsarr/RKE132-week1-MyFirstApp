9e75300a8150d49d9c498d8595d8a71a39fe9631
Risto Vatsar <r.vatsar@gmail.com>
2023-02-03 19:28:18 +0200

    first commit

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿//Comment
+
+
+using System.Net.Security;
+
+Console.WriteLine("Enter your name:");
+
+string userName = Console.ReadLine();
+
+//Console.WriteLine("Hello, " + userName + "!");
+
+//string interpolation $"Hello {userName}"
+
+Console.WriteLine($"Hello, {userName}!");
